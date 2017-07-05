7ae58750460e913e58a4610d41ef5d3d80824a8e
armanmiranda <arman.miranda@adish.co.jp>
2017-07-05 14:19:27 +0800

    fix table formatting, removal of known dead links and other fixes.

--- app/helpers/departments_helper.rb
+++ app/helpers/departments_helper.rb
@@ -1,11 +1,13 @@
 module DepartmentsHelper
-  def list_all_users
+  def list_all_users(model)
     html_output = ""
-    @department.users.each do |user|
+
+    model.users.each do |user|
       html_output << "<td>#{user.last_name}</td>"
       html_output << "<td>#{user.first_name}</td>"
       html_output << "<td>#{user.position}</td>"
     end
+
     html_output
   end
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,5 @@
 class Department < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true
 end
