4c3e7587bb45a0dac93e2280dbe00ba8c21635d5
armanmiranda <arman.miranda@adish.co.jp>
2017-07-06 08:51:25 +0800

    add a search functionality in the users index page

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+#Searching feature gem
+gem 'ransack'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,12 @@ class User < ApplicationRecord
   def email_changed?
     false
   end
+
+  ransacker :full_name do |parent|
+    Arel::Nodes::InfixOperation.new('||',
+      Arel::Nodes::InfixOperation.new('||',
+        parent.table[:first_name], Arel::Nodes.build_quoted(' ')
+      ),
+      parent.table[:last_name])
+  end
 end
